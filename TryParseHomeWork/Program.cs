a55618dc6963a86e08e0a8d8f51e172a71a4f56c
Reijer59 <48212731+Reijer59@users.noreply.github.com>
2023-12-15 18:46:31 +0100

    Add project files.

---
+++ TryParseHomeWork/Program.cs
@@ -0,0 +1,11 @@
+﻿// Source Code
+// Section Common Variables
+// Type Conversions trail
+
+int maandenGewerkt = 12;
+decimal maandSalaris = 2200.12M;
+decimal jaarSalaris;
+
+jaarSalaris = maandSalaris * maandenGewerkt;
+
+Console.WriteLine(jaarSalaris.ToString());
